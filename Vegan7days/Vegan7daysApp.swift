d653a74badf4cd74a45b383fb2135f570a7c5644
carahsu <cheerful528@gmail.com>
2025-02-21 14:43:19 +0800

    Initial Commit

---
+++ Vegan7days/Vegan7daysApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vegan7daysApp.swift
+//  Vegan7days
+//
+//  Created by Cara Hsu on 2025/2/21.
+//
+
+import SwiftUI
+
+@main
+struct Vegan7daysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
