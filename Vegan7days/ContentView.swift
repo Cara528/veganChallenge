a548718ccd48d4d1531e6a8c29721be1285746d8
carahsu <cheerful528@gmail.com>
2025-03-14 15:05:18 +0800

    Initial commit

--- Vegan7days/ContentView.swift
+++ Vegan7days/ContentView.swift
@@ -9,15 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ZStack {
+                // 背景圖片
+                
+                VStack {
+                    
+                    Image("backgroundImageName") // 記得將背景圖片加到你的資源中
+                        .resizable()
+                    
+                    NavigationLink {
+                        SecondView()
+                    } label: {
+                        Text("旅程開始!")
+                            .font(.largeTitle)
+                    }
+                }
+            }
+//            .navigationTitle("首頁")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
+        
     }
 }
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
 
 #Preview {
     ContentView()
--- Vegan7days/SecondView.swift
+++ Vegan7days/SecondView.swift
@@ -4,4 +4,101 @@
 //
 //  Created by Cara Hsu on 2025/2/21.
 //
+//
 
+import SwiftUI
+
+var progress: CGFloat = 0
+
+var percentage: Int = 0
+
+struct SecondView: View {
+    
+    let days = [
+        
+        "DAY1",
+        "DAY2",
+        "DAY3",
+        "DAY4",
+        "DAY5",
+        "DAY6",
+        "DAY7",
+        "DAY8"
+        
+    ]
+    
+    var body: some View {
+        
+        VStack {
+        
+            HStack{
+                Text("每日挑戰")
+                    .font(.title)
+                
+                ZStack{
+                    Circle()
+                        .stroke(lineWidth: 10)
+                        .opacity(0.3)
+                        .foregroundColor(Color.gray)
+                    
+                    Circle()
+                        .trim(from: 0.0, to: progress)
+                        .stroke(
+                            AngularGradient(gradient: Gradient(colors: [.purple, .red]), center: .center),
+                            style: StrokeStyle(lineWidth: 8, lineCap: .round)
+                        )
+                        .rotationEffect(.degrees(-90))
+                        
+                    VStack{
+                        Text("目前進度")
+                            
+                        
+                        Text("\(percentage)%")
+                            .bold()
+                    }
+
+                }
+                .frame(width: 100, height: 100)
+            }
+                
+            NavigationStack {
+                
+                let columns = [GridItem(), GridItem()]
+                ScrollView {
+                    LazyVGrid(columns: columns) {
+                        ForEach(days.indices, id: \.self) { item in
+                            NavigationLink(destination: ChallengeView(day: days[item])){
+                                DailyView(day: days[item])
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct DailyView: View {
+    let day: String
+    
+    var body: some View {
+        
+            ZStack{
+                Image(day)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width:200, height: 200)
+                    .cornerRadius(10)
+                
+                Text(day)
+                    .font(.system(size: 20))
+                    .cornerRadius(10)
+        }
+    }
+}
+
+
+
+#Preview {
+    SecondView()
+}
